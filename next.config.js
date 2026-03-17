25cbfe9eecf390911c83b750d86e4e2c3c98c3fd
successmlm01 <successmlm01@gmail.com>
2026-03-17 16:48:34 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   env: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
   },
